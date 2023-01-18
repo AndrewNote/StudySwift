a0937e8bcb94ca20f2ba93d5cf634b8c3e903528
AndrewNote <whk1140@gmail.com>
2023-01-18 14:30:02 +0900

    Configure Project

---
+++ Instargram/Instargram/Controller/MainTabController.swift
@@ -0,0 +1,14 @@
+//
+//  MainTabController.swift
+//  Instargram
+//
+//  Created by Andrew on 2023/01/18.
+//
+
+import UIKit
+
+class MainTabController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
