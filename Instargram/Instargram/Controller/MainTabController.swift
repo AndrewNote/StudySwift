efdd14aa03a7b8589477e34ca422cdd876cd9d03
AndrewNote <whk1140@gmail.com>
2023-01-20 19:25:38 +0900

    Instargram: Finish Tab Bar Setup

--- Instargram/Instargram/Controller/MainTabController.swift
+++ Instargram/Instargram/Controller/MainTabController.swift
@@ -17,16 +17,25 @@ class MainTabController: UITabBarController {
     
     // MARK: - Helpers
     func configureViewControllers() {
-        let feed = FeedController()     // 상수에 뷰 컨트롤러의 인스턴스를 저장 하는것
-        let search = SearchController()
-        let imageSelector = ImageSelectorController()
-        let notifications = NotificationController()
-        let profile = ProfileController()
+        // feed ViewController에 navigationController이 내장되어 있다.
+        let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected") , rootViewController: FeedController())    // 상수에 뷰 컨트롤러의 인스턴스를 저장 하는것
+        let search = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected") , rootViewController: SearchController())
+        let imageSelector = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected") , rootViewController: ImageSelectorController())
+        let notifications = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected") , rootViewController: NotificationController())
+        let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected") , rootViewController: ProfileController())
         
         // viewControllers가 tabBarController를 상속해서 얻는기능이라고 한다.
-        // 인스턴스를 바로 넣을 수 있지만 ex) FeedController() 일단 비추 한다고 한다.
+        // 인스턴스를 바로 넣을 수 있지만 ex) FeedController() 비추임. templateNavigationController 함수를 넣어서 옵션값을 넣으면 배열안이 지저분해 질거임
         viewControllers = [feed, search, imageSelector, notifications, profile]
+        tabBar.tintColor = .black
     }
     
+    func templateNavigationController(unselectedImage: UIImage, selectedImage: UIImage, rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.tabBarItem.image = unselectedImage
+        nav.tabBarItem.selectedImage = selectedImage
+        nav.navigationBar.tintColor = .black
+        return nav
+    }
     
 }
