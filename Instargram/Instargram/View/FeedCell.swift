7ddc7b5a11ee00edb1cf4ece638a332d71b66a9d
AndrewNote <whk1140@gmail.com>
2023-01-25 01:47:07 +0900

    Instargram: Finishing Up Feed Cell UI 1/3

--- Instargram/Instargram/View/FeedCell.swift
+++ Instargram/Instargram/View/FeedCell.swift
@@ -25,6 +25,60 @@ class FeedCell: UICollectionViewCell {
         return button
     }()
     
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.isUserInteractionEnabled = true
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        return iv   // UIImageView 반환
+    }()
+    
+    private let likeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "like_unselected"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private let commentButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "comment"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private let shareButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "send2"), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private let likesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1 like"
+        label.font = .boldSystemFont(ofSize: 12)
+        return label
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Some test caption for now..."
+        label.font = .boldSystemFont(ofSize: 12)
+        return label
+    }()
+    
+    private let postTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2 days ago"
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = .lightGray
+//      tintColor와 textColor의 차이는
+        return label
+    }()
+    
+    // MARK: Action
     @objc private func didTapUsername() {
         print("DEBUG: did tap username")
     }
@@ -35,6 +89,13 @@ class FeedCell: UICollectionViewCell {
         backgroundColor = .white
         addSubview(profileImageView)
         addSubview(usernameButton)
+        addSubview(postImageView)
+        addSubview(likeButton)
+        addSubview(shareButton)
+        addSubview(commentButton)
+        addSubview(likesLabel)
+        addSubview(captionLabel)
+        addSubview(postTimeLabel)
         
         profileImageView.translatesAutoresizingMaskIntoConstraints = false
         profileImageView.topAnchor.constraint(equalTo: topAnchor, constant: 12).isActive = true
@@ -46,6 +107,11 @@ class FeedCell: UICollectionViewCell {
         usernameButton.translatesAutoresizingMaskIntoConstraints = false
         usernameButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor).isActive = true
         usernameButton.leftAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 10).isActive = true
+        
+        postImageView.translatesAutoresizingMaskIntoConstraints = false
+        postImageView.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8).isActive = true
+        postImageView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        postImageView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
     }
     
     // 필수 초기화
