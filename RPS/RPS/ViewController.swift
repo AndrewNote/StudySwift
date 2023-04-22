afcda8a58c56d632d7c2d5cfd4f5c71440786184
AndrewNote <whk1140@gmail.com>
2023-04-22 23:16:33 +0900

    feat: RPS 생성

---
+++ RPS/RPS/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Andrew on 2023/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+    }
+
+
+}
+
