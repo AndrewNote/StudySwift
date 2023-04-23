68a54c3cb26cfa2e39e3dcba770de8e648600acd
AndrewNote <whk1140@gmail.com>
2023-04-23 23:51:34 +0900

    feat: startButton 생성 및 ResetButton로 변경 이벤트 추가

--- RPS/RPS/ViewController.swift
+++ RPS/RPS/ViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var startButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Start", for: .normal)
+        button.backgroundColor = .green
+        button.addTarget(self, action: #selector(didTapStartButton), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc private func didTapStartButton() {
+        if startButton.backgroundColor == .green {
+            startButton.backgroundColor = .red
+            startButton.setTitle("Reset", for: .normal)
+        } else {
+            startButton.backgroundColor = .green
+            startButton.setTitle("Start", for: .normal)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
@@ -16,8 +35,13 @@ class ViewController: UIViewController {
     
     private func configureUI() {
         view.backgroundColor = .white
+        view.addSubview(startButton)
+        
+        NSLayoutConstraint.activate([
+            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            startButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50),
+            startButton.widthAnchor.constraint(equalToConstant: 200),
+            startButton.heightAnchor.constraint(equalToConstant: 100)
+        ])
     }
-
-
 }
-
