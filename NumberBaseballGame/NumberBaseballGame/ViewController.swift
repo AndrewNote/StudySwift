bc58435cd9052e9524c7ac42db21864d70f7b010
AndrewNote <whk1140@gmail.com>
2023-04-25 21:47:25 +0900

    feat: 기회를 알려주는 chanceLabel 추가

--- NumberBaseballGame/NumberBaseballGame/ViewController.swift
+++ NumberBaseballGame/NumberBaseballGame/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var chance = 9
+    
+    private lazy var chanceLabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Chance: \(String(chance))"
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
@@ -16,7 +25,12 @@ class ViewController: UIViewController {
     
     private func configureUI() {
         view.backgroundColor = .white
+        view.addSubview(chanceLabel)
         
+        NSLayoutConstraint.activate([
+            chanceLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            chanceLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+        ])
     }
 
 }
