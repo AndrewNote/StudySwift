215d6c795cf32f9137ee5bb130f3594f7191dc3c
AndrewNote <whk1140@gmail.com>
2023-04-25 21:42:26 +0900

    feat: NumberBaseballGame 프로젝트 추가, 스토리보드삭제, configureUI 메서드 추가

---
+++ NumberBaseballGame/NumberBaseballGame/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NumberBaseballGame
+//
+//  Created by Andrew on 2023/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+        
+    }
+
+}
