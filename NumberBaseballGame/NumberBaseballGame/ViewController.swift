cb2c71ca00bfe562593da315329aee07dcd41b24
AndrewNote <whk1140@gmail.com>
2023-04-28 23:29:23 +0900

    feat: 랜덤한 1부터 9까지의 중복되지 않는 숫자 생성하는 함수 구현

--- NumberBaseballGame/NumberBaseballGame/ViewController.swift
+++ NumberBaseballGame/NumberBaseballGame/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class ViewController: UIViewController {
 
     private var chance = 9
+    private let imageViewNames = ["circle", "triangle", "prohibition"]
+    private var timer: Timer?
+    var spinCount = 0
     
     private lazy var chanceLabel = {
         let label = UILabel()
@@ -18,18 +21,88 @@ class ViewController: UIViewController {
         return label
     }()
     
+    private let leftImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "prohibition")
+        return imageView
+    }()
+    private let centerImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "prohibition")
+        return imageView
+    }()
+    private let rightImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "prohibition")
+        return imageView
+    }()
+    
+    private lazy var slotImageViewStackView = {
+        let stackView = UIStackView(arrangedSubviews: [leftImageView, centerImageView, rightImageView])
+        stackView.axis = .horizontal
+        stackView.distribution = .equalCentering
+        stackView.spacing = 10
+        return stackView
+    }()
+    
+    private lazy var mainStackView = {
+        let stackView = UIStackView(arrangedSubviews: [chanceLabel, slotImageViewStackView, actionButton])
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let actionButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .darkGray
+        return button
+    }()
+    
+    @objc private func didTapButton() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+        actionButton.isEnabled = false
+    }
+    
+    @objc func update() {
+            for _ in 0..<imageViewNames.count {
+                let imageName = imageViewNames.randomElement()!
+                centerImageView.image = UIImage(named: imageName)
+            }
+            
+            spinCount += 1
+            
+            if spinCount >= 20 {
+                timer?.invalidate()
+                timer = nil
+                actionButton.isEnabled = true
+            }
+        }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        
+        var numbers = [Int]()
+        while numbers.count < 3 {
+            let randomNumber = Int.random(in: 1...9)
+            if !numbers.contains(randomNumber) {
+                numbers.append(randomNumber)
+            }
+        }
     }
     
     private func configureUI() {
-        view.backgroundColor = .white
-        view.addSubview(chanceLabel)
+        view.backgroundColor = .systemBackground
+        view.addSubview(mainStackView)
         
         NSLayoutConstraint.activate([
-            chanceLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            chanceLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            mainStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            slotImageViewStackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            slotImageViewStackView.heightAnchor.constraint(equalToConstant: 200)
         ])
     }
 
