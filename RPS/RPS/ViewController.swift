7b8312bc56b8532f18818c2f0545fbc61565419d
AndrewNote <whk1140@gmail.com>
2023-05-01 19:55:22 +0900

    feat: RPS 필요한 Label, Button, StackView, ImageView 생성

--- RPS/RPS/ViewController.swift
+++ RPS/RPS/ViewController.swift
@@ -8,40 +8,98 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    private lazy var startButton = {
+    
+    private let gameTitleLabel = {
+        let label = UILabel()
+        label.text = "묵찌빠 게임"
+        return label
+    }()
+    
+    private let userLabel = {
+        let label = UILabel()
+        label.text = "User"
+        return label
+    }()
+    
+    private let computerLabel = {
+        let label = UILabel()
+        label.text = "Computer"
+        return label
+    }()
+    
+    private let userImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    private let computerImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    private let startOrSeleteButton = {
         let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Start", for: .normal)
-        button.backgroundColor = .green
-        button.addTarget(self, action: #selector(didTapStartButton), for: .touchUpInside)
+        button.backgroundColor = .blue
         return button
     }()
     
-    @objc private func didTapStartButton() {
-        if startButton.backgroundColor == .green {
-            startButton.backgroundColor = .red
-            startButton.setTitle("Reset", for: .normal)
-        } else {
-            startButton.backgroundColor = .green
-            startButton.setTitle("Start", for: .normal)
-        }
-    }
+    private let resetButton = {
+        let button = UIButton()
+        button.setTitle("Reset", for: .normal)
+        button.backgroundColor = .red
+        return button
+    }()
+    
+    private let labelStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private let imageStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private let mainStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
     }
     
+    private func configureStackView() {
+        labelStackView.addArrangedSubview(userLabel)
+        labelStackView.addArrangedSubview(computerLabel)
+        
+        imageStackView.addArrangedSubview(userImageView)
+        imageStackView.addArrangedSubview(computerImageView)
+        
+        mainStackView.addArrangedSubview(gameTitleLabel)
+        mainStackView.addArrangedSubview(labelStackView)
+        mainStackView.addArrangedSubview(imageStackView)
+        mainStackView.addArrangedSubview(startOrSeleteButton)
+        mainStackView.addArrangedSubview(resetButton)
+    }
+    
     private func configureUI() {
         view.backgroundColor = .white
-        view.addSubview(startButton)
-        
-        NSLayoutConstraint.activate([
-            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            startButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50),
-            startButton.widthAnchor.constraint(equalToConstant: 200),
-            startButton.heightAnchor.constraint(equalToConstant: 100)
-        ])
+        startOrSeleteButton.addTarget(self, action: #selector(didTapStartButton), for: .touchUpInside)
+    }
+    
+    @objc private func didTapStartButton() {
+        if startOrSeleteButton.backgroundColor == .blue {
+            startOrSeleteButton.backgroundColor = .green
+            startOrSeleteButton.setTitle("Selete", for: .normal)
+        } else {
+            startOrSeleteButton.backgroundColor = .blue
+            startOrSeleteButton.setTitle("Start", for: .normal)
+        }
     }
 }
