ab2d1e989c02b872de835ac3fa6901445ad88c9c
AndrewNote <whk1140@gmail.com>
2023-05-08 23:30:19 +0900

    feat: GameSelectionScreen 파일 생성

---
+++ MiniGameHeaven/MiniGameHeaven/GameSelectionScreen.swift
@@ -0,0 +1,18 @@
+//
+//  GameSelectionScreen.swift
+//  MiniGameHeaven
+//
+//  Created by Andrew on 2023/05/08.
+//
+
+import UIKit
+
+class GameSelectionScreen: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension GameSelectionScreen: UITableViewDiffableDataSource<<#SectionIdentifierType: Hashable & Sendable#>, <#ItemIdentifierType: Hashable & Sendable#>> {
+    
+}
