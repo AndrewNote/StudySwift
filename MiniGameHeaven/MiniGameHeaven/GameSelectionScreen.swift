d9a31d320fb75f1285a80005cbdc324fe8e00437
AndrewNote <whk1140@gmail.com>
2023-05-09 21:56:08 +0900

    test: UICollectionViewDiffableDataSource 테스트

--- MiniGameHeaven/MiniGameHeaven/GameSelectionScreen.swift
+++ MiniGameHeaven/MiniGameHeaven/GameSelectionScreen.swift
@@ -7,7 +7,27 @@
 
 import UIKit
 
+
+enum Section: CaseIterable {
+    case main
+}
+
+struct Mountain: Hashable {
+    let name: String
+    let height: Int
+    let identifier = UUID()  // Item identifier는 unique해야 하므로
+}
+
 class GameSelectionScreen: UIViewController {
+    
+    private let dataSource: UICollectionViewDiffableDataSource<Section, Mountain>
+    private let mainCollectionView = {
+        let collectionView = UICollectionView()
+        return collectionView
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -16,3 +36,4 @@ class GameSelectionScreen: UIViewController {
 extension GameSelectionScreen: UITableViewDiffableDataSource<<#SectionIdentifierType: Hashable & Sendable#>, <#ItemIdentifierType: Hashable & Sendable#>> {
     
 }
+
