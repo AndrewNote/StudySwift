4580b7a976dbaa10dd5a9b3d47e4b4328c82182c
AndrewNote <whk1140@gmail.com>
2023-06-25 17:58:52 +0900

    feat: Instargram 프로젝트 생성

---
+++ Instargram/Instargram/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Instargram
+//
+//  Created by Andrew on 2023/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+}
+
