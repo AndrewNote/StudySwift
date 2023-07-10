76f8b782a8fe46848ae57a62cd8733a275f0ec59
AndrewNote <whk1140@gmail.com>
2023-07-10 12:06:24 +0900

    feat: Instagram ProfileController 기본 collectionView 생성

--- Instagram/Instagram/Controller/ProfileController.swift
+++ Instagram/Instagram/Controller/ProfileController.swift
@@ -7,8 +7,25 @@
 
 import UIKit
 
+struct ProfileItem: Hashable {
+    let userName: String
+    let profileImage: UIImageView
+    let postCount: Int
+    let followerCount: Int
+    let followingCount: Int
+    let uuid: UUID
+}
+
 class ProfileController: UIViewController {
     // MARK: Properties
+    private var dataSoruce: UICollectionViewDiffableDataSource<Section, ProfileItem>?
+    private let dummyData = [ProfileItem(userName: "Andrew", profileImage: UIImageView(), postCount: 9, followerCount: 3, followingCount: 0, uuid: UUID())]
+    
+    private lazy var collectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createFeedLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
     
     // MARK: LifeCycle
     override func viewDidLoad() {
@@ -20,4 +37,39 @@ class ProfileController: UIViewController {
     private func configureUI() {
         view.backgroundColor = .systemBackground
     }
+}
+
+extension ProfileController {
+    // MARK: Layout
+    private func createFeedLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                             heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalHeight(1.0))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                         subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+    
+        // MARK: DataSource
+    private func setUpDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<FeedCell, ProfileItem> { (cell, indexPath, item) in
+            
+        }
+        
+        dataSoruce = UICollectionViewDiffableDataSource<Section, ProfileItem>(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, ProfileItem>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(dummyData)
+        dataSoruce?.apply(snapshot, animatingDifferences: true)
+    }
+
 }
