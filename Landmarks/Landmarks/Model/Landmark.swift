7dbcc437e86cd6b091d1d6f636fc55abccb05f0b
AndrewNote <whk1140@gmail.com>
2023-07-14 15:44:50 +0900

    feat: Landmarks 랜드마크 모델 생성 및 폴더로 파일분리

--- Landmarks/Landmarks/Model/Landmark.swift
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -6,6 +6,7 @@
 //
 import Foundation
 import SwiftUI
+import CoreLocation
 
 struct Landmark: Hashable, Codable {
     var id: Int
@@ -17,6 +18,11 @@ struct Landmark: Hashable, Codable {
     private var imageName: String
     var image: Image {
         Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
     }
     
     struct Coordinates: Hashable, Codable {
---
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -0,0 +1,32 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Andrew on 2023/07/14.
+//
+
+import Foundation
+
+private let landmarks: [Landmark] = load("landmarkData.json")
+
+private func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) is main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
