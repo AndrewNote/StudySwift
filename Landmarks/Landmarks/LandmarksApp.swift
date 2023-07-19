827d27e4946afda715051440107540e12da62743
AndrewNote <whk1140@gmail.com>
2023-07-19 20:53:03 +0900

    Feat: Landmarks Adopt the Model Object in Your Views

--- Landmarks/Landmarks/LandmarksApp.swift
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 @main   // 앱의 시작점?
 struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+    
     // body 속성은 하나 이상의 장면을 반환
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
