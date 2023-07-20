5f6c79d3b1ad6885c56c37d4ee8cb930e3e3bcae
AndrewNote <whk1140@gmail.com>
2023-07-20 14:51:59 +0900

    init: PokemonCollection

---
+++ PokemonCollection/PokemonCollection/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PokemonCollection
+//
+//  Created by Andrew on 2023/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    // MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: Method
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+
+}
+
