30a545cdba163f5e6e819fea54ac4ee31296ba8e
AndrewNote <whk1140@gmail.com>
2023-07-20 18:49:33 +0900

    feat: PokemonCollection PokemonCell UI 프로퍼티 추가 및 오토레이아웃 설정

--- PokemonCollection/PokemonCollection/PokemonCell.swift
+++ PokemonCollection/PokemonCollection/PokemonCell.swift
@@ -10,10 +10,24 @@ import UIKit
 class PokemonCell: UICollectionViewCell {
     
     // MARK: Properties
+    private let pokemonImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .blue
+        return imageView
+    }()
+    
+    private let pokemonNameLabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Pokemon Name"
+        return label
+    }()
     
     // MARK: LifeCycle
     override init(frame: CGRect) {
         super.init(frame: .zero)
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
@@ -23,5 +37,17 @@ class PokemonCell: UICollectionViewCell {
     // MARK: Method
     private func configureUI() {
         backgroundColor = .tertiarySystemGroupedBackground
+        addSubview(pokemonImageView)
+        addSubview(pokemonNameLabel)
+        
+        NSLayoutConstraint.activate([
+            pokemonImageView.widthAnchor.constraint(equalTo: widthAnchor),
+            pokemonImageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.8),
+            pokemonNameLabel.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.2),
+            pokemonNameLabel.topAnchor.constraint(equalTo: pokemonImageView.bottomAnchor),
+        ])
+        
+        self.layer.cornerRadius = 10
+        self.layer.masksToBounds = true
     }
 }
