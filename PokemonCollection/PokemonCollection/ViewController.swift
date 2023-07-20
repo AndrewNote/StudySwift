0b632f25d1279e6696402a5e1283287787ebb760
AndrewNote <whk1140@gmail.com>
2023-07-20 15:44:35 +0900

    feat: PokemonCollection CollectionView 생성 및 Navigation 설정

--- PokemonCollection/PokemonCollection/ViewController.swift
+++ PokemonCollection/PokemonCollection/ViewController.swift
@@ -10,17 +10,62 @@ import UIKit
 class ViewController: UIViewController {
     
     // MARK: Properties
+    private let collectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
     
     // MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
+        configureNavigation()
         configureUI()
     }
     
     // MARK: Method
+    private func configureNavigation() {
+        title = "포켓몬 도감"
+        navigationController?.navigationBar.tintColor = .systemBackground
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.backgroundColor = .systemOrange
+        navigationController?.navigationBar.standardAppearance = navigationBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.search, target: self, action: #selector(didTapRightButton))
+        
+    }
+    
     private func configureUI() {
         view.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
+    
+    // MARK: Selector
+    @objc private func didTapRightButton() {
+        
+    }
+}
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 99
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    
 }
 
