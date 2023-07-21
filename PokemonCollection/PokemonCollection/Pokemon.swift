dbd627a66e26b5d24ae4db4621fe6408bb3838ba
AndrewNote <whk1140@gmail.com>
2023-07-21 22:47:56 +0900

    feat: PokemonCollection Pokemon 모델 생성

---
+++ PokemonCollection/PokemonCollection/Pokemon.swift
@@ -0,0 +1,22 @@
+//
+//  Pokemon.swift
+//  PokemonCollection
+//
+//  Created by Andrew on 2023/07/21.
+//
+
+import Foundation
+
+// JSON 형태의 데이터를 받아오는데 Swift에서 사용할 수 있게 변환시켜줌
+
+struct Pokemon: Codable {
+    let name: String?
+    let imageUrl: String?
+    let id: Int?
+    let weight: Int?
+    let height: Int?
+    let defense: Int?
+    let attack: Int?
+    let description: String?
+    let type: String?
+}
---
+++ PokemonCollection/PokemonCollection/PokemonService.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonService.swift
+//  PokemonCollection
+//
+//  Created by Andrew on 2023/07/21.
+//
+
+import Foundation
+
+// HTTP 통신을 하기 위해서는 URL이 필요
+
+class PokemonService {
+    let url = ""
+    
+    func fetchPokemons() {
+        if let url = URL(string: url) {
+            URLSession.shared.
+        }
+    }
+}
