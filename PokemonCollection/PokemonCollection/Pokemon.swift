51475f44d6607bd53680d907e9aa357515ebe621
AndrewNote <whk1140@gmail.com>
2023-07-22 20:19:03 +0900

    fix: PokemonCollection 네트워크 통신을 하기 위해 Pokemon 모델 변경

--- PokemonCollection/PokemonCollection/Pokemon.swift
+++ PokemonCollection/PokemonCollection/Pokemon.swift
@@ -9,14 +9,26 @@ import Foundation
 
 // JSON 형태의 데이터를 받아오는데 Swift에서 사용할 수 있게 변환시켜줌
 
+//struct Pokemon: Codable {
+//    let name: String?
+//    let imageUrl: String?
+//    let id: Int?
+//    let weight: Int?
+//    let height: Int?
+//    let defense: Int?
+//    let attack: Int?
+//    let description: String?
+//    let type: String?
+//}
+
+struct PokemonListResponse: Codable {
+    let count: Int
+    let next: String?
+    let previous: String?
+    let results: [Pokemon]
+}
+
 struct Pokemon: Codable {
-    let name: String?
-    let imageUrl: String?
-    let id: Int?
-    let weight: Int?
-    let height: Int?
-    let defense: Int?
-    let attack: Int?
-    let description: String?
-    let type: String?
+    let name: String
+    let url: String
 }
