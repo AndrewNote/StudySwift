0ddf3c50edfd7ce57bf51f2101c224bf69907459
AndrewNote <whk1140@gmail.com>
2023-08-01 23:23:15 +0900

    add: Landmarks hikeData.json 파일 추가 및 Hike 모델 데이터에 생성

--- Landmarks/Landmarks/Model/ModelData.swift
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -10,6 +10,7 @@ import Combine
 
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
 }
 
 let landmarks: [Landmark] = load("landmarkData.json")
