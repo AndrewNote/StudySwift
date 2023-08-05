e421109af6472b5d14ff5c1b431db84987db72c1
AndrewNote <whk1140@gmail.com>
2023-08-05 19:05:39 +0900

    feat: Landmarks Add a Category View

---
+++ Landmarks/Landmarks/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Andrew on 2023/08/05.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
