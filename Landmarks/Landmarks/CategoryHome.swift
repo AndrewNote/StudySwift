122a5944f048f998eb0923ef48a330f14304bc58
AndrewNote <whk1140@gmail.com>
2023-08-05 19:09:51 +0900

    feat: Landmarks Create a Category List

--- Landmarks/Landmarks/CategoryHome.swift
+++ Landmarks/Landmarks/CategoryHome.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
     var body: some View {
-        NavigationView {
-            Text("Hello, World!")
-                .navigationTitle("Featured")
-        }
-    }
+         NavigationView {
+             List {
+                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                     Text(key)
+                 }
+             }
+             .navigationTitle("Featured")
+         }
+     }
+    
 }
 
 struct CategoryHome_Previews: PreviewProvider {
