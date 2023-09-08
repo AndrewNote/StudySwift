9e61bcf2dd82133efa0fb886871473f02cc6ac94
ChanHyuc <whk1140@gmail.com>
2023-09-08 15:04:21 +0900

    Style: 코드 분리 extension을 사용해서 body에서 분리

--- SweetSwiftUI/SweetSwiftUI/ProductRow.swift
+++ SweetSwiftUI/SweetSwiftUI/ProductRow.swift
@@ -6,28 +6,30 @@
 //
 
 import SwiftUI
+// 작은 뷰 하나를 만들어서 재사용하는게 목표 UIKit에 TableViewCell과 같은거
 
 struct ProductRow: View {
     let product: Product
     
     var body: some View {
-        HStack {
+        HStack {    // View의 가장 바깥쪽 부분
             productImage
             productDescription
         }
-        
     }
-    
-    var productImage: some View {
+}
+
+private extension ProductRow {
+    var productImage: some View {   // 상품 이미지
         Image(product.imageName)
             .resizable()
             .scaledToFill()
-            .frame(width: 140)
-            .clipped()
+            .frame(width: 140)  // 이미지 가로 140
+            .clipped()          // 뷰를 벗어나는 이미지를 자름
     }
     
-    var productDescription: some View {
-        VStack(alignment: .leading) {    // 상품명, 상품설명
+    var productDescription: some View { // 상품명, 상품설명
+        VStack(alignment: .leading) {
             Text(product.name)
                 .font(.headline)
                 .fontWeight(.medium)
@@ -36,31 +38,32 @@ struct ProductRow: View {
             Text(product.description)
                 .font(.footnote)
                 .foregroundColor(Color.secondary)
-            
+
             Spacer()
-            footerView
+            footerView  // 가격정보, 버튼
             
         }
         .padding([.leading, .bottom], 12)
         .padding([.top, .trailing])
+        // 상하좌우 각각 여백을 줄 방향을 선택하고 숫자를 적는다
+        // 상하를 한꺼번에 줄 때 .vertical 좌우는 .horizontal도 가능
     }
     
-    var footerView: some View {
-        HStack(spacing: 0) {    // 가격정보, 버튼
+    var footerView: some View { // 가격정보, 버튼
+        HStack(spacing: 0) {
             Text("₩").font(.footnote)
             Text("\(product.price)").font(.headline)
             Spacer()
             Image(systemName: "heart")
                 .imageScale(.large)
                 .foregroundColor(Color.peach)
-            // 에셋에 포함해 둔 peach 사용
+                // 에셋에 넣어둔 peach 컬러 사용
                 .frame(width: 32, height: 32)
             Image(systemName: "cart")
                 .foregroundColor(Color("peach"))
                 .frame(width: 32, height: 32)
         }
     }
-    
 }
 
 struct ProductRow_Previews: PreviewProvider {
