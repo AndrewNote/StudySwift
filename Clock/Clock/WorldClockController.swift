86d7cbdf944dd9a3fdbd088298c876a02086c840
ChanHyuc <whk1140@gmail.com>
2024-04-11 21:19:36 +0900

    Feat: Clock WorldClock Delete 기능 추가

--- Clock/Clock/WorldClockController.swift
+++ Clock/Clock/WorldClockController.swift
@@ -73,4 +73,11 @@ extension WorldClockController: UITableViewDataSource {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            worldClockList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
 }
